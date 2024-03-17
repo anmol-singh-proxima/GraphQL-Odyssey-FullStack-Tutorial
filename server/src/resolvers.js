619b7e3062fb1486f9b28bd544d489e94d52fc1c
anmol-singh-proxima <kumaranmol888@gmail.com>
2024-03-17 20:20:47 +0530

    Made changes in schema, resolvers and dataSources for fetching details of a single Track

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -5,11 +5,17 @@ const resolvers = {
         // Returns an Array of Tracks
         tracksForHome: (_, __, {dataSources}) => {
             return dataSources.trackAPI.getTracksForHome();
+        },
+        track: (_, {id}, {dataSources}) => {
+            return dataSources.trackAPI.getTrack(id);
         }
     },
     Track: {
         author: ({authorId}, _, {dataSources}) => {
             return dataSources.trackAPI.getAuthor(authorId);
+        },
+        modules: ({id}, _, {dataSources}) => {
+            return dataSources.trackAPI.getTrackModules(id);
         }
     }
 }
